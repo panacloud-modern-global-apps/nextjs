5b6c43d71d121b14913e4fc1c2329d54887cd047
abdulwaqar844 <abdulwaqar844@gmail.com>
2022-07-13 00:16:55 +0500

    api updated

--- step15_fetching_data_ssr_rest/pages/index.tsx
+++ step15_fetching_data_ssr_rest/pages/index.tsx
@@ -1,42 +1,45 @@
-import type { NextPage } from 'next';
-import Link from 'next/link';
-import axios from 'axios';
+import type { NextPage } from "next";
+import Link from "next/link";
+import axios from "axios";
 
 //https://nextjs.org/docs/basic-features/typescript
-import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next';
+import { GetStaticProps, GetStaticPaths, GetServerSideProps } from "next";
 
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const usersReq = await axios.get('https://api.rwnjs.com/04/users')
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/users"
+  );
+  // console.log(data);
   return {
     props: {
-      users: usersReq.data
-    }
-  }
-}
-
-
+      users: data,
+    },
+  };
+};
 
 //https://stackoverflow.com/questions/69560905/how-to-type-a-page-component-with-props-in-next-js
 //https://nextjs.org/docs/api-reference/data-fetching/get-initial-props#typescript
 //https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props
+type User = {
+  id: number;
+  email: String;
+  username: String;
+  name: String;
+};
 const Home: NextPage<GetServerSideProps> = (props: GetServerSideProps) => {
   let users = props.users;
   return (
     <ul>
-    {
-      users.map((user) =>
+      {users.map((user: User) => (
         <li key={user.id}>
-          <Link href={`/users/${user.username}`} passHref >
-            <a> {user.username} </a>
+          <Link href={`/users/${user.id}`}>
+            <a> {user.name} </a>
           </Link>
+          <p> {user.username}</p>
         </li>
-      )
-    }
+      ))}
     </ul>
-  )
-}
+  );
+};
 
 export default Home;
-
-
